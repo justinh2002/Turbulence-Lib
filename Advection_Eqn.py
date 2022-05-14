5edd70caa77c5da1dd637f7640e7a1133cc14168
justinh2002 <94345578+justinh2002@users.noreply.github.com>
2022-05-14 17:11:08 +1000

    Update Advection_Eqn.py

--- Advection_Eqn.py
+++ Advection_Eqn.py
@@ -1,3 +1,10 @@
+# Finite difference methods to solve Riemann wave problems, from personal private CFD code repo
+
+
 def lax_wendroff(u): 
     u[1:-1] = c/2.0*(1+c)*u[:-2] + (1-c**2)*u[1:-1] - c/2.0*(1-c)*u[2:]
     return u[1:-1]
+
+
+# u denotes the flux variable
+
